4877de57b31d16794c4a81da311ddea024cb320b
cclamb <cclamb@unm.edu>
2013-06-30 10:59:52 -0700

    work

--- exp_ccl/dynamic_hub.py
+++ exp_ccl/dynamic_hub.py
@@ -2,6 +2,7 @@
 import pdb
 
 from pox.core import core
+import pox.openflow.libopenflow_01 as of
 
 
 log = core.getLogger()
@@ -9,9 +10,12 @@ log = core.getLogger()
 
 def formedness_check(f):
     def new_f(obj, event):
-        log.info("entering: " + f.__name__)
+        log.info("checking packet formedness for: " + f.__name__)
+        packet = event.parsed
+        if not packet.parsed:
+            log.warning('packet is improperly formed for: ' + f.__name__)
+            return
         f(obj, event)
-        log.info("exited: " + f.__name__)
     return new_f
 
 
@@ -19,6 +23,16 @@ class HubTrait:
 
     @formedness_check
     def handle_packet(self, event):
+        packet = event.parsed
+        packet_in = event.ofp
+
+        msg = of.ofp_packet_out()
+        msg.data = packet_in
+
+        action = of.ofp_action_output(port = of.OFPP_ALL)
+        msg.actions.append(action)
+
+        self.connection.send(msg)
         log.info('processing event')
         
 
