a1b5fea787ff2f04f501c5103704d350a889ffb2
cclamb <cclamb@unm.edu>
2013-07-01 06:18:01 -0700

    work on L2 learning switch

--- exp_ccl/dynamic_evaluator.py
+++ exp_ccl/dynamic_evaluator.py
@@ -3,7 +3,9 @@ import pdb
 
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
-
+from pox.lib.util import dpid_to_str
+from pox.lib.util import str_to_bool
+import time
 
 log = core.getLogger()
 
@@ -19,10 +21,10 @@ def formedness_check(f):
     return new_f
 
 
-class HubTrait:
+class HubTrait(object):
 
     @formedness_check
-    def handle_packet(self,ctx, event):
+    def handle_packet(self, ctx, event):
         packet = event.parsed
         packet_in = event.ofp
 
@@ -35,6 +37,36 @@ class HubTrait:
         connection = ctx['connection']
         connection.send(msg)
         log.info('processing event')
+
+
+class L2LearningSwitchTrait(HubTrait):
+
+    def __init__(self):
+        self.mac_to_port = {}
+        super(L2LearningSwitchTrait, self).__init__()
+
+    def _is_local_traffic(self, packet):
+        return packet.type == packet.LLDP_TYPE or packet.dst.isBridgeFiltered()
+
+    @formedness_check
+    def handle_packet(self, ctx, event):
+        #pdb.set_trace()
+        msg = of.ofp_packet_out()
+        
+        packet = event.parsed
+
+        self.mac_to_port[packet.src] = event.port
+
+        # This could also be done in a decorator
+        if self._is_local_traffic(packet):
+            # this is much more complex in example.
+            return
+
+        if packet.dst.is_multicast:
+            HubTrait.handle_packet({'connection': ctx['connection']}, event)
+            return
+
+        return
         
 
 class DynamicEvaluator:
@@ -56,6 +88,6 @@ def launch():
     log.info('launching.')
     def start_hub(event):
         log.info('starting hub.')
-        DynamicEvaluator(event.connection, HubTrait())
+        DynamicEvaluator(event.connection, L2LearningSwitchTrait())
     core.openflow.addListenerByName('ConnectionUp', start_hub)
       
