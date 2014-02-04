3ded64510c3c615461eea5fba9f83b99be053901
lambo <cclamb@ece.unm.edu>
2014-02-04 05:31:28 -0800

    working on output to files form VMs and monitoring

--- bin/single_switch_cmd.py
+++ bin/single_switch_cmd.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
-from time import sleep
+from time import sleep, time
+from select import poll, POLLIN
+from subprocess import Popen, PIPE
+from signal import SIGINT
 
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.util import dumpNodeConnections
+from mininet.util import dumpNodeConnections, pmonitor
 from mininet.log import setLogLevel
 
 
@@ -66,6 +69,83 @@ def testSendCmds(net):
     print "\tWhat do the host say?"
     print results
 
+def monitorFiles(outfiles, seconds, timeoutms):
+    """Monitor a set of files and return information from them."""
+    devnull = open('/dev/null', 'w')
+    tails, fdToFile, fdToHost = {}, {}, {}
+    for h, outfile in outfiles.iteritems():
+        tail = Popen(['tail', '-f', outfile], stdout = PIPE, stderr = devnull)
+        fd = tail.stdout.fileno()
+        tails[h] = tail
+        fdToFile[fd] = tail.stdout
+        fdToHost[fd] = h
+
+    # Prepare to poll output files
+    readable = poll()
+    for t in tails.values():
+        readable.register(t.stdout.fileno(), POLLIN)
+        
+    endTime = time() + seconds
+    while time() < endTime:
+        fdlist = readable.poll(timeoutms)
+        if fdlist:
+            for fd, _flags, in fdlist:
+                f = fdToFile[fd]
+                host = fdToHost[fd]
+                line = f.readline().strip()
+                yield host, line
+        else:
+            yield None, ''
+
+    for t in tails.values():
+        t.terminate()
+
+    devnull.close()
+
+def testMonitor(net, n = 3, seconds = 3):
+    """This will fail with the terminating pingall, so comment out prior to running."""
+    hosts = net.hosts
+    server = hosts[0]
+    outfiles, errfiles = {}, {}
+    for h in hosts:
+        outfiles[h] = '/tmp/%s.out' % (h.name)
+        errfiles[h] = '/tmp/%s.err' % (h.name)
+        h.cmd('echo > ', outfiles[h])
+        h.cmd('echo > ', errfiles[h])
+
+        # Start pinging.
+        h.cmdPrint('ping', server.IP(),
+                   '>', outfiles[h],
+                   '2>', errfiles[h],
+                   '&')
+
+    print 'Monitoring output for', seconds, 'seconds'
+    for h, line in monitorFiles(outfiles, seconds, timeoutms = 500):
+        if h:
+            print '%s, %s' % (h.name, line)
+
+    for h in hosts:
+        h.cmd('kill %ping')
+
+    net.stop()
+
+def testMonitorSimple(net, n = 3, seconds = 10):
+    hosts = net.hosts
+    server = hosts[0]
+    popens = {}
+    for h in hosts:
+        popens[h] = h.popen('ping', server.IP())
+
+    print 'Monitoring output for', seconds, 'seconds'
+    endTime = time() + seconds
+    for h, line in pmonitor(popens, timeoutms = 500):
+        if h:
+            print '%s: %s' % (h.name, line)
+        if time() >= endTime:
+            for p in popens.values():
+                p.send_signal(SIGINT)
+    net.stop()
+
 def simpleTest():
     """Create and test a simple network"""
     topo = SingleSwitchTopo(n = 5)
@@ -74,11 +154,11 @@ def simpleTest():
     print "Dumping host connections..."
     dumpNodeConnections(net.hosts)
 
-    testSendCmds(net)
+    testMonitorSimple(net)
 
-    print "Testing network connectivity..."
-    net.pingAll()
-    net.stop()
+    #print "Testing network connectivity..."
+    #net.pingAll()
+    #net.stop()
 
 if __name__ == '__main__':
     setLogLevel('output')
