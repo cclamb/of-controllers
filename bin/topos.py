14d809bf7b3585221bdee61bdc5f52d9310d5da4
lambo <cclamb@ece.unm.edu>
2014-02-22 17:10:58 -0800

    hub working

--- bin/topos.py
+++ bin/topos.py
@@ -6,23 +6,6 @@ from mininet.topolog import LinearTopo
 from mininet.cli import CLI
 
 
-class LinearTopology:
-
-    def __init__(self, k=2, n=1, **opts):
-        self.topology =  LinearTopo(k, n, **opts)
-
-    def configure_mininet(self):
-        self.net = Mininet(self.topo)
-
-    def start(self):
-        self.net.start()
-
-    def test(self):
-        self.net.pingAll()
-        
-    def stop(self):
-        self.net.stop()
-
 if __name__ == '__main__':
     setLogLevel('info')
     
